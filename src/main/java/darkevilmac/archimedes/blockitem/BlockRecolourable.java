c7e4920c85573d4596a542c65d347043b3c91106
darkevilmac <bk1325@gmail.com>
2015-06-06 13:08:34 -0700

    Make BlockRecolourable make more sense (im from canada eh COLOUR with a U)

--- src/main/java/darkevilmac/archimedes/blockitem/BlockRecolourable.java
+++ src/main/java/darkevilmac/archimedes/blockitem/BlockRecolourable.java
@@ -6,6 +6,7 @@ import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.ItemDye;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.world.World;
@@ -18,9 +19,23 @@ public class BlockRecolourable extends BlockColored {
 
     @Override
     public boolean onBlockActivated(World world, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
-        if (world != null && !world.isRemote && playerIn != null && !playerIn.isSneaking()) {
-            if (playerIn.getItemInUse() != null && playerIn.getItemInUse().getItem() != null && playerIn.getItemInUse().getItem() instanceof ItemDye) {
-                world.setBlockState(pos, state.withProperty(BlockColored.COLOR, EnumDyeColor.byDyeDamage(playerIn.getItemInUse().getItemDamage()).getMetadata()));
+        if (world != null && playerIn != null && !playerIn.isSneaking()) {
+            IBlockState blockState = world.getBlockState(pos);
+            ItemStack heldItem = playerIn.getHeldItem();
+            if (blockState == null || heldItem == null) return false;
+
+            if (heldItem.getItem() != null && heldItem.getItem() instanceof ItemDye) {
+                if (EnumDyeColor.byDyeDamage(heldItem.getItemDamage()).getMapColor() != ((EnumDyeColor) blockState.getValue(COLOR)).getMapColor()) {
+                    if (!world.isRemote) {
+                        blockState = blockState.withProperty(BlockColored.COLOR, EnumDyeColor.byDyeDamage(playerIn.getHeldItem().getItemDamage()));
+                        world.setBlockState(pos, blockState);
+                        if (!playerIn.capabilities.isCreativeMode)
+                            playerIn.getHeldItem().stackSize -= 1;
+                    } else {
+                        playerIn.swingItem();
+                    }
+                    return true;
+                }
             }
         }
 
