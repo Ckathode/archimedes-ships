e2e7e7b4e3b6a671d0222f3c80e0509a47ea9a6d
darkevilmac <bk1325@gmail.com>
2016-07-08 16:54:15 -0700

    Small model changes, Helm model still broken.

--- src/main/java/darkevilmac/archimedes/client/render/TileEntityHelmRenderer.java
+++ src/main/java/darkevilmac/archimedes/client/render/TileEntityHelmRenderer.java
@@ -14,10 +14,13 @@ import net.minecraft.client.renderer.block.model.ModelRotation;
 import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.client.renderer.texture.TextureMap;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
+import net.minecraft.client.resources.IReloadableResourceManager;
+import net.minecraft.client.resources.IResourceManager;
+import net.minecraft.client.resources.IResourceManagerReloadListener;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ResourceLocation;
-import net.minecraftforge.client.model.Attributes;
 import net.minecraftforge.client.model.IModel;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
 import net.minecraftforge.common.model.TRSRTransformation;
@@ -36,7 +39,8 @@ import darkevilmac.movingworld.common.tile.IMovingWorldTileEntity;
 
 public class TileEntityHelmRenderer extends TileEntitySpecialRenderer {
 
-    public static HashMap<EnumFacing, IBakedModel> helmModels;
+    public static ReloadListener reloadListener;
+    protected static HashMap<EnumFacing, IBakedModel> helmModels;
     Function<ResourceLocation, TextureAtlasSprite> textureGetter = new Function<ResourceLocation, TextureAtlasSprite>() {
         public TextureAtlasSprite apply(ResourceLocation location) {
             return Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(location.toString());
@@ -53,15 +57,18 @@ public class TileEntityHelmRenderer extends TileEntitySpecialRenderer {
             try {
                 model = ModelLoaderRegistry.getModel(new ResourceLocation(ArchimedesShipMod.RESOURCE_DOMAIN + "block/helmWheel"));
             } catch (Exception e) {
-                ArchimedesShipMod.modLog.error("A critical exception occured when rendering a helm model, " + e.getLocalizedMessage());
+                ArchimedesShipMod.modLog.error("A critical exception occurred when rendering a helm model, " + e.getLocalizedMessage());
             }
 
             if (model != null) {
-                helmModels.put(EnumFacing.NORTH, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 0)), Attributes.DEFAULT_BAKED_FORMAT, textureGetter));
-                helmModels.put(EnumFacing.SOUTH, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 180)), Attributes.DEFAULT_BAKED_FORMAT, textureGetter));
-                helmModels.put(EnumFacing.WEST, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, -90)), Attributes.DEFAULT_BAKED_FORMAT, textureGetter));
-                helmModels.put(EnumFacing.EAST, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 90)), Attributes.DEFAULT_BAKED_FORMAT, textureGetter));
+                helmModels.put(EnumFacing.NORTH, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 0)), DefaultVertexFormats.BLOCK, textureGetter));
+                helmModels.put(EnumFacing.SOUTH, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 180)), DefaultVertexFormats.BLOCK, textureGetter));
+                helmModels.put(EnumFacing.WEST, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, -90)), DefaultVertexFormats.BLOCK, textureGetter));
+                helmModels.put(EnumFacing.EAST, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 90)), DefaultVertexFormats.BLOCK, textureGetter));
             }
+
+            reloadListener = new ReloadListener();
+            ((IReloadableResourceManager) Minecraft.getMinecraft().getResourceManager()).registerReloadListener(reloadListener);
         }
 
         try {
@@ -95,9 +102,7 @@ public class TileEntityHelmRenderer extends TileEntitySpecialRenderer {
             GlStateManager.shadeModel(7424);
         }
 
-        GlStateManager.pushMatrix();
-
-        float newX, newY, newZ = 0;
+        float newX, newY, newZ;
         newX = (float) x;
         newY = (float) y;
         newZ = (float) z;
@@ -107,7 +112,7 @@ public class TileEntityHelmRenderer extends TileEntitySpecialRenderer {
             bindTexture(TextureMap.LOCATION_BLOCKS_TEXTURE);
             modelBaked = helmModels.get(blockStateFacing);
         }
-        vertexBuffer.begin(GL11.GL_QUADS, Attributes.DEFAULT_BAKED_FORMAT);
+        vertexBuffer.begin(GL11.GL_QUADS, DefaultVertexFormats.BLOCK);
         GlStateManager.translate(newX, newY, newZ);
 
         if (modelBaked != null) {
@@ -137,12 +142,32 @@ public class TileEntityHelmRenderer extends TileEntitySpecialRenderer {
                 vertexBuffer.color(1.0F, 1.0F, 1.0F, 1.0F);
                 vertexBuffer.addVertexData(vd);
             }
-
         }
 
         tessellator.draw();
         RenderHelper.enableStandardItemLighting();
         GlStateManager.popMatrix();
-        GlStateManager.popMatrix();
+    }
+
+    public class ReloadListener implements IResourceManagerReloadListener {
+        @Override
+        public void onResourceManagerReload(IResourceManager resourceManager) {
+            helmModels.clear();
+
+            IModel model = null;
+
+            try {
+                model = ModelLoaderRegistry.getModel(new ResourceLocation(ArchimedesShipMod.RESOURCE_DOMAIN + "block/helmWheel"));
+            } catch (Exception e) {
+                ArchimedesShipMod.modLog.error("A critical exception occurred when rendering a helm model, " + e.getLocalizedMessage());
+            }
+
+            if (model != null) {
+                helmModels.put(EnumFacing.NORTH, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 0)), DefaultVertexFormats.BLOCK, textureGetter));
+                helmModels.put(EnumFacing.SOUTH, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 180)), DefaultVertexFormats.BLOCK, textureGetter));
+                helmModels.put(EnumFacing.WEST, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, -90)), DefaultVertexFormats.BLOCK, textureGetter));
+                helmModels.put(EnumFacing.EAST, model.bake(new TRSRTransformation(ModelRotation.getModelRotation(0, 90)), DefaultVertexFormats.BLOCK, textureGetter));
+            }
+        }
     }
 }
